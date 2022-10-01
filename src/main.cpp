6f115ffa15b52c57c6aa7efc78d2163a6eeccfc6
dpeddi <1086815+dpeddi@users.noreply.github.com>
2022-10-01 22:13:56 +0200

    some cleanup and optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -25,10 +25,17 @@ DmxInput dmxInput[2];
 #define START_CHANNEL 1
 #define NUM_CHANNELS 8192
 
-//volatile uint16_t buffer_cs1[DMXINPUT_BUFFER_SIZE(START_CHANNEL, NUM_CHANNELS)];
-//volatile uint16_t buffer_cs2[DMXINPUT_BUFFER_SIZE(START_CHANNEL, NUM_CHANNELS)];
 volatile uint16_t buffer_cs1[2][12*123*10];
-//volatile uint16_t buffer_cs2[12*123*10];
+
+void intelaced_FillScreen() {
+  tft.fillScreen(TFT_BLACK);
+  for (uint y = 0; y < tft.height(); y++) {
+    for (uint x = 0; x < tft.width(); x++) {
+      if (x % ZOOM_X  == 0 && y % ZOOM_Y == 0)
+        tft.drawPixel(x, y, TFT_ORANGE);
+    }
+  }
+}
 
 void setup()
 {
@@ -36,44 +43,47 @@ void setup()
   // delay(2000);
   //Serial.setTimeout(50);
   Serial.println("juno g lcd emulator");
-	  tft.init();
-	//tft.setRotation(2);
-	tft.setRotation(1);
+  tft.init();
+  //tft.setRotation(2);
+  tft.setRotation(1);
 #ifdef DRAW_SPLASH
-	tft.fillScreen(TFT_ORANGE);
-	drawBitmapZoom(0, 0,  (const uint8_t *)junog, 240, 90, TFT_BLACK);
+  tft.fillScreen(TFT_ORANGE);
+  drawBitmapZoom(0, 0, (const uint8_t *)junog, 240, 90, TFT_BLACK);
   delay(500);
 #endif
 
+
+
 #define DRAW_INFO
 #ifdef DRAW_INFO
-	tft.fillScreen(TFT_ORANGE);
-	tft.setTextColor(TFT_BLACK);
-	tft.setCursor(0, 280, 2);
-	tft.println("Roland Juno G Lcd Emulator v0.1");
-  tft.print("CPU_FREQ:" + String(rp2040.f_cpu()));
-  delay(1000);
+  intelaced_FillScreen();
+  tft.setTextColor(TFT_BLACK);
+  tft.setCursor(0, tft.height()/2 -10  , 2);
+  tft.setTextSize(2);
+  
+  tft.setTextDatum(TC_DATUM);
+  tft.drawString("Roland Juno G Lcd Emulator v0.1", tft.width() /2, tft.height() / 2 - 20 );
+  tft.drawString("CPU_FREQ:" + String(rp2040.f_cpu()), tft.width() /2, tft.height() / 2 + 10 );
+  delay(500);
 #endif
 
 #ifdef DRAW_PINOUT  
-	drawPinout(1000);
+  drawPinout(1000);
 #endif
-	
-	tft.fillScreen(TFT_BLACK);
-	tft.setCursor(0, 0, 2);
-	tft.setTextSize(1);
-	tft.print("DMX:");
-	
-	
-    // Setup our DMX Input to read on GPIO 0, from channel 1 to 3
-    dmxInput[0].begin(2, START_CHANNEL, NUM_CHANNELS, pio0, 1);
-    dmxInput[1].begin(2, START_CHANNEL, NUM_CHANNELS, pio1, 2);
-    dmxInput[0].read_async(buffer_cs1[0]);
-    dmxInput[1].read_async(buffer_cs1[1]);
+  
+  intelaced_FillScreen();
 
-    // Setup the onboard LED so that we can blink when we receives packets
-    pinMode(LED_BUILTIN, OUTPUT);
+  tft.setCursor(0, 0, 2);
+  tft.setTextSize(1);
+  
+  // Setup our DMX Input to read on GPIO 0, from channel 1 to 3
+  dmxInput[0].begin(2, START_CHANNEL, NUM_CHANNELS, pio0, 1);
+  dmxInput[1].begin(2, START_CHANNEL, NUM_CHANNELS, pio1, 2);
+  dmxInput[0].read_async(buffer_cs1[0]);
+  dmxInput[1].read_async(buffer_cs1[1]);
 
+  // Setup the onboard LED so that we can blink when we receives packets
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 volatile int start_index[2] = {0,0};
@@ -81,68 +91,47 @@ volatile uint8_t page[2] = {0,0};
 volatile uint8_t xx[2] = {0,0};
 
 void draw_juno_g(uint8_t val, uint8_t rs, uint8_t cs) {
-
-    if (rs == 0) {
+  if (rs == 0) {
 #ifdef SHOWCMD
-        showcmd( cs, val );
+    showcmd( cs, val );
 #endif
-        if ((val >> 4) == 0xb) {
-          page[cs] = val & 0xf;
-          xx[cs] = 0;
-        } /*else
-        if (tft.getCursorY() >320 ) {
-            tft.fillScreen(TFT_ORANGE);
-            tft.setCursor(0,0,2);
-        } */
-    } else if (rs == 1 ) {
-        if (xx[cs] < 120) {  // avoid overlap the right area
-          for (int i = 0; i < 8; i++ ) {
-              if (((val >> i) & 0x1) == 1) {
-                tft.drawPixel(120 * cs * ZOOM_X + xx[cs] * ZOOM_X, (page[cs] * 8 + i ) * ZOOM_Y, TFT_BLACK);
-              } else {
-                tft.drawPixel(120 * cs * ZOOM_X + xx[cs] * ZOOM_X, (page[cs] * 8 + i ) * ZOOM_Y, TFT_ORANGE);
-              }
-          }
-          xx[cs]++;
-        } 
+    if ((val >> 4) == 0xb) {
+      page[cs] = val & 0xf;
+      xx[cs] = 0;
+    } 
+  } else if (rs == 1 ) {
+    if (xx[cs] < 120) {  // avoid overlap the right area
+      for (int i = 0; i < 8; i++ ) {
+        if (((val >> i) & 0x1) == 1) {
+          tft.drawPixel(120 * cs * ZOOM_X + xx[cs] * ZOOM_X, (page[cs] * 8 + i ) * ZOOM_Y, TFT_BLACK);
+        } else {
+          tft.drawPixel(120 * cs * ZOOM_X + xx[cs] * ZOOM_X, (page[cs] * 8 + i ) * ZOOM_Y, TFT_ORANGE);
+        }
+      }
+      xx[cs]++;
     }
-
+  }
 }
 
 void loop()
 {
+  if(millis() > 50 + dmxInput[0].latest_packet_timestamp() && millis() > 50 + dmxInput[1].latest_packet_timestamp() ) {
+    //Serial.println("no data!");
+    return;
+  } 
+  for (uint i = 0; i < 123*12; i++)  
+  {
+    for (uint8_t j = 0; j <2; j++) {
+      uint8_t val = buffer_cs1[j][i] & 0xff;
+      uint8_t rs = (buffer_cs1[j][i] >> 9) & 1;
+      draw_juno_g(val, rs, j);
+    }
+  }
 
-    //delay(30);
-
-    if(millis() > 50 + dmxInput[0].latest_packet_timestamp() && millis() > 50 + dmxInput[1].latest_packet_timestamp() ) {
-        //Serial.println("no data!");
-        return;
-    } 
-
-
-      for (uint i = 0; i < 123*12; i++)  
-      {
-
-        for (uint8_t j = 0; j <2; j++) {
-
-
-          char sbuf[50];
-
-          uint8_t val = buffer_cs1[j][i] & 0xff;
-          uint8_t rs = (buffer_cs1[j][i] >> 9) & 1;
-           
-          draw_juno_g(val, rs, j);
-        }
-      }
-
-
-    // Wait for next DMX packet
-    //dmxInput[0].read(buffer);
-
-    // Blink the LED to indicate that a packet was received
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(10);
-    digitalWrite(LED_BUILTIN, LOW);
+  // Blink the LED to indicate that a packet was received
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(10);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 
